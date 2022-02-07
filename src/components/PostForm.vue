f6624e87496089e94f2d661f17f8ab43be3651cb
Vladislav Valeev <soulproxy0404@gmail.com>
2022-02-07 15:30:25 +0300

    Second commit

--- src/components/PostForm.vue
+++ src/components/PostForm.vue
@@ -1,13 +1,42 @@
 <template>
-$END$
+  <form>
+    <h4>Создание поста</h4>
+    <my-input v-model="post.title" type="text" placeholder="Название"/>
+    <my-input v-model="post.body" type="text" placeholder="Описание"/>
+    <my-button @click="createPost">Создать</my-button>
+  </form>
 </template>
 
 <script>
 export default {
-name: "PostForm"
+  data() {
+    return {
+      post: {
+        title: '',
+        body: ''
+      }
+    }
+  },
+  methods: {
+    createPost() {
+      this.post.id = Date.now();
+      this.$emit('create', this.post)
+      this.post = {
+        title: '',
+        body: ''
+      }
+    },
+  }
 }
 </script>
 
 <style scoped>
+form {
+  display: flex;
+  flex-direction: column;
+}
 
+.btn {
+  margin-top: 15px;
+}
 </style>
---
+++ src/components/UI/MyPageWrapper.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="page__wrapper">
+    <div v-for="pageNumber in totalPages" :key="page" class="page"
+         :class="{'current-page': page === pageNumber }" @click="$emit('changePage', pageNumber)">{{ pageNumber }}</div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "MyPageWrapper",
+  props: {
+    totalPages: {
+      default: 0
+    },
+    page: {
+      default: 1
+    }
+  },
+}
+</script>
+
+<style scoped>
+.page__wrapper {
+  display: flex;
+  margin-top: 15px;
+}
+.page {
+  border: 1px solid black;
+  padding: 10px;
+}
+.current-page {
+  border: 3px solid teal;
+}
+</style>
